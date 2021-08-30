7fa1e897ee36ec2e950023def2b72c48a35253af
shuaiyin <shuaiyin.top@foxmail.com>
2021-08-30 17:34:39 +0800

    测试部署后页面空白的问题

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   // 路径代理
   resolve: {
-    base: './',
     alias: [
       { find: '@', replacement: '/src' }
     ]
